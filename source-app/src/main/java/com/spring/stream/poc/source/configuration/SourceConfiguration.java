69fecf7a710219dde169fa57def23156dd511f80
Fabio Maia <fabioromm@gmail.com>
2018-04-18 09:58:28 -0300

    Used rabbitmq instead of kinesis for testing the app works

--- source-app/src/main/java/com/spring/stream/poc/source/configuration/SourceConfiguration.java
+++ source-app/src/main/java/com/spring/stream/poc/source/configuration/SourceConfiguration.java
@@ -1,25 +1,11 @@
 package com.spring.stream.poc.source.configuration;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder;
-import com.amazonaws.services.kinesis.AmazonKinesisAsync;
-import com.amazonaws.services.kinesis.AmazonKinesisAsyncClientBuilder;
 import com.spring.stream.poc.source.TimerDataSource;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.stream.binder.kinesis.properties.KinesisBinderConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.aws.metadata.DynamoDbMetaDataStore;
-import org.springframework.integration.metadata.MetadataStore;
-
-import static com.amazonaws.regions.Regions.DEFAULT_REGION;
 
 @Configuration
-@EnableConfigurationProperties({KinesisBinderConfigurationProperties.class})
+//@EnableConfigurationProperties({KinesisBinderConfigurationProperties.class})
 public class SourceConfiguration {
 
     @Bean
@@ -27,39 +13,39 @@ public class SourceConfiguration {
         return new TimerDataSource();
     }
 
-    @Bean
-    public AmazonKinesisAsync amazonKinesis() {
-        return AmazonKinesisAsyncClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("", "")))
-                .withClientConfiguration(
-                        new ClientConfiguration()
-                                .withMaxErrorRetry(0)
-                                .withConnectionTimeout(1000))
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration("http://localhost:4568", DEFAULT_REGION.getName()))
-                .build();
-    }
-
-    @Bean
-    public MetadataStore kinesisCheckpointStore(KinesisBinderConfigurationProperties configurationProperties) {
-        AmazonDynamoDBAsync dynamoDB = AmazonDynamoDBAsyncClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("", "")))
-                .withClientConfiguration(
-                        new ClientConfiguration()
-                                .withMaxErrorRetry(0)
-                                .withConnectionTimeout(1000))
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration("http://localhost:4569", DEFAULT_REGION.getName()))
-                .build();
-
-        KinesisBinderConfigurationProperties.Checkpoint checkpoint = configurationProperties.getCheckpoint();
-
-        DynamoDbMetaDataStore kinesisCheckpointStore = new DynamoDbMetaDataStore(dynamoDB, checkpoint.getTable());
-        kinesisCheckpointStore.setReadCapacity(checkpoint.getReadCapacity());
-        kinesisCheckpointStore.setWriteCapacity(checkpoint.getWriteCapacity());
-        kinesisCheckpointStore.setCreateTableDelay(checkpoint.getCreateDelay());
-        kinesisCheckpointStore.setCreateTableRetries(checkpoint.getCreateRetries());
-
-        return kinesisCheckpointStore;
-    }
+//    @Bean
+//    public AmazonKinesisAsync amazonKinesis() {
+//        return AmazonKinesisAsyncClientBuilder.standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("", "")))
+//                .withClientConfiguration(
+//                        new ClientConfiguration()
+//                                .withMaxErrorRetry(0)
+//                                .withConnectionTimeout(1000))
+//                .withEndpointConfiguration(
+//                        new AwsClientBuilder.EndpointConfiguration("http://localhost:4568", DEFAULT_REGION.getName()))
+//                .build();
+//    }
+//
+//    @Bean
+//    public MetadataStore kinesisCheckpointStore(KinesisBinderConfigurationProperties configurationProperties) {
+//        AmazonDynamoDBAsync dynamoDB = AmazonDynamoDBAsyncClientBuilder.standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("", "")))
+//                .withClientConfiguration(
+//                        new ClientConfiguration()
+//                                .withMaxErrorRetry(0)
+//                                .withConnectionTimeout(1000))
+//                .withEndpointConfiguration(
+//                        new AwsClientBuilder.EndpointConfiguration("http://localhost:4569", DEFAULT_REGION.getName()))
+//                .build();
+//
+//        KinesisBinderConfigurationProperties.Checkpoint checkpoint = configurationProperties.getCheckpoint();
+//
+//        DynamoDbMetaDataStore kinesisCheckpointStore = new DynamoDbMetaDataStore(dynamoDB, checkpoint.getTable());
+//        kinesisCheckpointStore.setReadCapacity(checkpoint.getReadCapacity());
+//        kinesisCheckpointStore.setWriteCapacity(checkpoint.getWriteCapacity());
+//        kinesisCheckpointStore.setCreateTableDelay(checkpoint.getCreateDelay());
+//        kinesisCheckpointStore.setCreateTableRetries(checkpoint.getCreateRetries());
+//
+//        return kinesisCheckpointStore;
+//    }
 }
